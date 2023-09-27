eda15ca5cdcc90aa6cb11128cba8b162704b4005
IAM-AM <aliaksandr.manko1@gmail.com>
2023-09-28 00:26:02 +0400

    refactored

--- conftest.py
+++ conftest.py
@@ -26,9 +26,4 @@ def setup_browser(request):
     browser = Browser(Config(driver))
     yield browser
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
-
     browser.quit()
